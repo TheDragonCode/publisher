c44bb340b8bc8dd22aa227d8ac1970e9f392b99f
Andrey Helldar <helldar@ai-rus.com>
2020-02-08 18:52:44 +0300

    Renamed attribute

--- src/Traits/Versionable.php
+++ src/Traits/Versionable.php
@@ -7,15 +7,15 @@ use Helldar\Publisher\Contracts\Version;
 trait Versionable
 {
     /** @var \Helldar\Publisher\Contracts\Version */
-    protected $version;
+    protected $version_concern;
 
     public function setVersionConcern(string $concern): void
     {
-        $this->version = $concern;
+        $this->version_concern = $concern;
     }
 
     protected function getVersionConcern(string $version = null): Version
     {
-        return new $this->version($version);
+        return new $this->version_concern($version);
     }
 }
